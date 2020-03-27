934bdf59eae8950ce6d8bc6c7d9715c2f228ee46
Ignat Insarov <kindaro@gmail.com>
2020-03-28 00:51:09 +0300

    Compute square root of 12 by binary search.

--- pi.ml
+++ pi.ml
@@ -10,7 +10,27 @@ let odd i = i*.2. +. 1. ;;
 
 let sum = fold_left (+.) 0. ;;
 
-let madhava i = -1.**i *. sqrt 12. /. (odd i *. 3.**i) ;;
+let average x y = (x +. y) /. 2. ;;
+
+let uncurry f (x, y) = f x y ;;
+
+let binary_approach_step f y left_bound right_bound =
+    let middle_value = average left_bound right_bound
+    in if y < f middle_value
+        then (left_bound, middle_value)
+        else (middle_value, right_bound) ;;
+
+let binary_approach f y e left_bound right_bound =
+    unfold
+        (fun (left_bound, right_bound) -> right_bound -. left_bound < e)
+        (fun bounds -> (bounds, uncurry (binary_approach_step f y) bounds))
+        (left_bound, right_bound) ;;
+
+let madhava i =
+    let sqrt12 =
+        let bounds_list = binary_approach (fun x -> x*.x) 12. (10. ** (-10.)) 1. 100.
+        in bounds_list |> rev |> hd |> fst
+    in -1.**i *. sqrt12 /. (odd i *. 3.**i) ;;
 
 let series f i = i |> range0 |> map f |> sum ;;
 

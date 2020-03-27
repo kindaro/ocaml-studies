dc66ad5b5b83c2b9cb329a39fa45ebefa4ff4ac7
Ignat Insarov <kindaro@gmail.com>
2020-03-28 01:36:05 +0300

    Use more advanced root finder.

--- pi.ml
+++ pi.ml
@@ -14,6 +14,8 @@ let average x y = (x +. y) /. 2. ;;
 
 let uncurry f (x, y) = f x y ;;
 
+let last xs = xs |> rev |> hd ;;
+
 let binary_approach_step f y left_bound right_bound =
     let middle_value = average left_bound right_bound
     in if y < f middle_value
@@ -26,10 +28,21 @@ let binary_approach f y e left_bound right_bound =
         (fun bounds -> (bounds, uncurry (binary_approach_step f y) bounds))
         (left_bound, right_bound) ;;
 
+let derivative f x =
+    let dx = 10. ** (-10.)
+    in (f (x +. dx) -. f x) /. dx ;;
+
+let tangent_approach_step f x = x -. (f x /. derivative f x) ;;
+
+let tangent_approach f y e =
+    let next y = tangent_approach_step f y
+    in unfold
+        (fun y -> (next y -. y) ** 2. < e ** 2.)
+        (fun y -> (y, next y))
+        y ;;
+
 let madhava i =
-    let sqrt12 =
-        let bounds_list = binary_approach (fun x -> x*.x) 12. (10. ** (-10.)) 1. 100.
-        in bounds_list |> rev |> hd |> fst
+    let sqrt12 = last (tangent_approach (fun x -> x*.x -. 12.) 1. (10. ** (-10.)))
     in -1.**i *. sqrt12 /. (odd i *. 3.**i) ;;
 
 let series f i = i |> range0 |> map f |> sum ;;
